a2a572ca7bfc7f6d702b2e61750f8163191d25d9
tharun220603 <135967679+tharun220603@users.noreply.github.com>
2023-12-07 12:04:52 +0530

    Update stringfunction.py

--- stringfunction.py
+++ stringfunction.py
@@ -1,37 +1,25 @@
 def uppercase(input_str):
     return input_str.upper()
-
 def lowercase(input_str):
     return input_str.lower()
-
 def capitalize(input_str):
     return input_str.capitalize()
-
 def reverse(input_str):
     return input_str[::-1]
-
 def title_case(input_str):
-    return input_str.title()
-
+    return input_str.title(
 def swapcase(input_str):
     return input_str.swapcase()
-
 def strip_whitespace(input_str):
     return input_str.strip()
-
 def count_vowels(input_str):
     vowels = "aeiouAEIOU"
     return sum(1 for char in input_str if char in vowels)
-
 def is_palindrome(input_str):
     cleaned_str = ''.join(char.lower() for char in input_str if char.isalnum())
     return cleaned_str == cleaned_str[::-1]
-
-# Main function
 def main():
     user_input = input("Enter a string: ")
-
-    # Dictionary mapping user choices to functions
     options = {
         '1': uppercase,
         '2': lowercase,
@@ -42,9 +30,7 @@ def main():
         '7': strip_whitespace,
         '8': count_vowels,
         '9': is_palindrome
-        # Add more functions and corresponding keys as needed
     }
-
     print("\nChoose a string operation:")
     print("1. Uppercase")
     print("2. Lowercase")
@@ -55,18 +41,12 @@ def main():
     print("7. Strip Whitespace")
     print("8. Count Vowels")
     print("9. Check Palindrome")
-    # Add more options as needed
-
     choice = input("Enter your choice (1-9): ")
-
-    # Get the selected function and apply it to the user input
     selected_function = options.get(choice, None)
-
     if selected_function:
         result = selected_function(user_input)
         print("Result:", result)
     else:
         print("Invalid choice. Please enter a number between 1 and 9.")
-
 if __name__ == "__main__":
     main()
